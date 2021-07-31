bbe803d657d5aa6869d4393f39e22675e0f355e3
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-07-30 21:36:25 -0300

    Update API documentation

--- src/main/java/labs/communication/api/controller/CommunicationController.java
+++ src/main/java/labs/communication/api/controller/CommunicationController.java
@@ -1,5 +1,6 @@
 package labs.communication.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import labs.communication.api.resource.CommunicationInput;
 import labs.communication.domain.entity.Communication;
 import labs.communication.domain.service.CommunicationCancellation;
@@ -25,18 +26,21 @@ public class CommunicationController {
     private final CommunicationCancellation cancellation;
 
     @PostMapping
+    @Operation(summary = "Schedule a communication.")
     public ResponseEntity<Communication> schedule(@Validated @RequestBody CommunicationInput input) {
         var communication = scheduling.execute(modelMapper.map(input, Communication.class));
         return ResponseEntity.ok(communication);
     }
 
     @GetMapping(path = {"/{id}"})
+    @Operation(summary = "Get a communication status.")
     public ResponseEntity<Communication> query(@PathVariable Long id) {
         var communication = query.execute(id);
         return ResponseEntity.of(communication);
     }
 
     @DeleteMapping(path = {"/{id}"})
+    @Operation(summary = "Cancel a communication.")
     public ResponseEntity<?> cancel(@PathVariable Long id) {
         cancellation.execute(id);
         return ResponseEntity.ok().build();
